1a51f4da9baca136eb6bfcc860bc32d1d806a968
Michael Lai <themichaellai@gmail.com>
2013-03-03 16:09:11 -0500

    Fixed user presentations index

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -15,11 +15,8 @@ class PresentationsController < ApplicationController
   end
 
   def index
-    if user_signed_in?
-      @presentations = current_user.presentations.all
-    else
-      redirect_to new_user_session_path
-    end
+    @user = User.find(params[:user_id])
+    @presentations = @user.presentations.all
   end
 
   def create
