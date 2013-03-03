6d07699f31b13326c26031d689364c0f217f1b00
Michael Lai <themichaellai@gmail.com>
2013-03-02 22:12:04 -0500

    Saving presentation works

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,5 +1,21 @@
 class PresentationsController < ApplicationController
   def new
-    @presentation = Presentation.new
+    if user_signed_in?
+      @presentation = Presentation.new
+    else
+      redirect_to root_path
+    end
+  end
+
+  def create
+    @presentation = Presentation.new(params[:presentation])
+    @presentation.user = current_user
+    if @presentation.save
+      flash[:notice] = 'Created presentation'
+      redirect_to new_presentations_path
+    else
+      flash[:notice] = 'Could not create presentation'
+      redirect_to new_presentations_path
+    end
   end
 end
